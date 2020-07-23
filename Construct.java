5f98d885bf7d76c2b28c8e0369eba2bfaf02e564
pavanichunduri <navyabio12@gmail.com>
2020-07-23 15:28:56 +0530

    First Commit

---
+++ Construct.java
@@ -0,0 +1,49 @@
+﻿import java.util.Scanner;
+
+import java.util.logging.LogManager;
+
+import java.util.logging.Logger;
+
+public class Construct
+
+{
+
+	int snd,are;
+
+	Construct(int std,int area)
+
+	{
+
+		snd=std;
+
+		are=area;
+
+	}
+
+	String calculate()
+
+	{
+
+		if(snd==1)
+
+			return("Cost: "+are*12000);
+
+		else if(snd==2)
+
+			return("Cost: "+are*15000);
+
+		else if(snd==3)
+
+			return("Cost:"+are*18000);
+
+		else if(snd==4)
+
+			return("Cost:"+are*25000);
+
+		else
+
+			return("invalid");
+
+	}
+
+}
---
+++ ConstructCost.java
@@ -0,0 +1,66 @@
+﻿package logger;
+
+import java.util.Scanner;
+
+import java.util.logging.LogManager;
+
+import java.util.logging.Logger;
+
+public class ConstructionCost
+ {
+
+	static
+
+	{
+
+		Logger logger=Logger.getLogger(ConstructionCost.class.getName());
+
+		Scanner sc=new Scanner(System.in);
+
+		int s,a;
+
+		String message;
+
+		logger.info("Enter Material Standard\n1.standard\n2.above standard\n3.high standard\n4.high standard and fully automated home");
+
+		s=sc.nextInt();
+
+		if(s==1||s==2||s==3||s==4)
+
+		{
+
+			logger.info("Enter sq.ft");
+
+			a=sc.nextInt();
+
+			Construct ob1=new Construct(s,a);
+
+			message=ob1.calculate();
+
+			logger.info(message);
+
+		}
+
+		else
+
+		{
+
+			logger.info("invalid selection");
+
+			java.lang.System.exit(0);
+
+		}
+
+	}
+
+	public static void main(String[] args)
+                 {
+
+		// TODO Auto-generated method stub
+
+
+
+	}
+
+
+}
